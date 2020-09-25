b85e8cd810767f35065af3d5b2e43dcf13c42eb8
Dan Johnson <danjohnsonnj@gmail.com>
2020-09-25 14:24:52 -0400

    Add LESS, better readability

--- dist/historic.df007ef4.js
+++ dist/historic.df007ef4.js
@@ -117,64 +117,79 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"index.js":[function(require,module,exports) {
-document.addEventListener("DOMContentLoaded", async () => {
-  const stateSelect = document.getElementById("StateSelect");
+})({"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
 
-  async function updateResults(location = stateSelect.value) {
-    const getCovidData = async function (state = location) {
-      const url = `https://api.covidtracking.com/v1/states/${state}/current.json`;
-      const response = await fetch(url);
-
-      if (response.status !== 200) {
-        console.log("Looks like there was a problem. Status Code: " + response.status);
-        return;
-      }
-
-      const data = await response.json();
-      return data;
-    };
-
-    const formatResults = function (r) {
-      const deprecatedData = ["checkTimeEt", "commercialScore", "dateChecked", "dateModified", "grade", "hash", "hospitalized", "negativeIncrease", "negativeRegularScore", "negativeScore", "posNeg", "positiveScore", "score", "total"];
-      const filteredResults = Object.keys(r).reduce((acc, key) => {
-        if (!deprecatedData.includes(key)) {
-          acc[key] = r[key] || "not reported";
-        }
-
-        return acc;
-      }, {});
-      const date = new Date(filteredResults.lastUpdateEt);
-      const updated = `Updated on ${date.toDateString()} at ${date.toLocaleTimeString()}.`;
-      const results = {
-        updated,
-        positiveCases: filteredResults.positiveCasesViral,
-        positiveIncrease: filteredResults.positiveIncrease,
-        recovered: filteredResults.recovered,
-        death: filteredResults.death,
-        deathIncrease: filteredResults.deathIncrease,
-        hospitalizedCurrently: filteredResults.hospitalizedCurrently,
-        hospitalizedIncrease: filteredResults.hospitalizedIncrease,
-        inIcuCurrently: filteredResults.inIcuCurrently
-      };
-      return results;
-    };
-
-    const fetchResponse = await getCovidData(location);
-    const formattedResults = formatResults(fetchResponse);
-    console.log(formattedResults);
-    Object.keys(formattedResults).forEach(key => {
-      document.getElementById(`${key}`).innerHTML = formattedResults[key];
-    });
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
   }
 
-  stateSelect.addEventListener("change", evt => {
-    console.log(evt.currentTarget.value);
-    updateResults(evt.currentTarget.value);
-  });
-  updateResults();
-});
-},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"historic.less":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -378,5 +393,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/bing-covid-info.e31bb0bc.js.map
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/historic.df007ef4.js.map
